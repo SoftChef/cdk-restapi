23af4a0e37abd8e6c6d366343be2533397801aac
MinChe Tsai <tirme0812@gmail.com>
2021-06-29 17:14:30 +0800

    Upgrade CDK v1.110.1

--- .projenrc.js
+++ .projenrc.js
@@ -4,7 +4,7 @@ const project = new AwsCdkConstructLibrary({
   author: 'softchef-iot-lab',
   authorEmail: 'poke@softchef.com',
   npmAccess: NpmAccess.PUBLIC,
-  cdkVersion: '1.110.0',
+  cdkVersion: '1.110.1',
   projenVersion: '0.24.12',
   initialVersion: '1.0.0',
   defaultReleaseBranch: 'main',
